c6f30a94edf5d7fe984caf76161905135fb3b454
chan <cham9994@gmail.com>
2022-08-18 16:08:26 +0900

    config: netlify proxy 설정 - toml 파일 생성

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -40,194 +40,201 @@ const SelectBox = () => {
 
   const gugunSelectHandler = (e: string) => {
     setSelectedGugun(e)
-    if (currentLocation.pathname === '/myplace') {
-      dispatch(myplaceHandler(e))
-    }
+    dispatch(myplaceHandler(e))
   }
 
   return (
     <div className="flex items-center justify-center p-6 gap-2">
-      <div className="w-full max-w-xs mx-auto">
-        <Listbox as="div" className="space-y-1" value={selectedSido} onChange={sidoSelectHandler}>
-          {({ open }) => (
-            <>
-              <Listbox.Label className="block text-sm leading-5 font-bold text-gray-700">
-                시도를 선택하세요!
-              </Listbox.Label>
-              <div className="relative">
-                <span className="inline-block w-full rounded-md shadow-sm">
-                  <Listbox.Button className="text-black cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                    <span className="block truncate">{selectedSido}</span>
-                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                      <svg
-                        className="h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        stroke="currentColor"
-                      >
-                        <path
-                          d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-                          strokeWidth="1.5"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                    </span>
-                  </Listbox.Button>
-                </span>
-
-                <Transition
-                  show={open}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                  className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
-                >
-                  <Listbox.Options
-                    static
-                    className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
-                  >
-                    {sidoName.map((sido) => (
-                      <Listbox.Option key={sido} value={sido}>
-                        {({ selected, active }) => (
-                          <div
-                            className={`${
-                              active ? 'text-white bg-blue-600' : 'text-gray-900'
-                            } cursor-default select-none relative py-2 pl-8 pr-4`}
+      {currentLocation.pathname !== '/favorite' && (
+        <>
+          <div className="w-full max-w-xs mx-auto">
+            <Listbox
+              as="div"
+              className="space-y-1"
+              value={selectedSido}
+              onChange={sidoSelectHandler}
+            >
+              {({ open }) => (
+                <>
+                  <Listbox.Label className="block text-sm leading-5 font-bold text-gray-700">
+                    시도를 선택하세요!
+                  </Listbox.Label>
+                  <div className="relative">
+                    <span className="inline-block w-full rounded-md shadow-sm">
+                      <Listbox.Button className="text-black cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                        <span className="block truncate">{selectedSido}</span>
+                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                          <svg
+                            className="h-5 w-5 text-gray-400"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="currentColor"
                           >
-                            <span
-                              className={`${
-                                selected ? 'font-semibold' : 'font-normal'
-                              } block truncate`}
-                            >
-                              {sido}
-                            </span>
-                            {selected && (
-                              <span
-                                className={`${
-                                  active ? 'text-white' : 'text-blue-600'
-                                } absolute inset-y-0 left-0 flex items-center pl-1.5`}
-                              >
-                                <svg
-                                  className="h-5 w-5"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                >
-                                  <path
-                                    fillRule="evenodd"
-                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                    clipRule="evenodd"
-                                  />
-                                </svg>
-                              </span>
-                            )}
-                          </div>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
-                </Transition>
-              </div>
-            </>
-          )}
-        </Listbox>
-      </div>
+                            <path
+                              d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                              strokeWidth="1.5"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </span>
+                      </Listbox.Button>
+                    </span>
 
-      {currentLocation.pathname !== '/total' && (
-        <div className="w-full max-w-xs mx-auto">
-          <Listbox
-            as="div"
-            className="space-y-1"
-            value={selectedGugun}
-            onChange={gugunSelectHandler}
-          >
-            {({ open }) => (
-              <>
-                <Listbox.Label className="block text-sm leading-5 font-bold text-gray-700">
-                  구군을 선택하세요!
-                </Listbox.Label>
-                <div className="relative">
-                  <span className="inline-block w-full rounded-md shadow-sm">
-                    <Listbox.Button className="text-black cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                      <span className="block truncate">
-                        {selectedGugun ? selectedGugun : sidoDustContents[0].stationName}
-                      </span>
-                      <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                        <svg
-                          className="h-5 w-5 text-gray-400"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          stroke="currentColor"
-                        >
-                          <path
-                            d="M7 7l3-3 3 3m0 6l-3 3-3-3"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </span>
-                    </Listbox.Button>
-                  </span>
-
-                  <Transition
-                    show={open}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                    className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
-                  >
-                    <Listbox.Options
-                      static
-                      className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                    <Transition
+                      show={open}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
                     >
-                      {sidoDustContents?.map((item: any, index: number) => (
-                        <Listbox.Option key={index} value={item.stationName}>
-                          {({ selected, active }) => (
-                            <div
-                              className={`${
-                                active ? 'text-white bg-blue-600' : 'text-gray-900'
-                              } cursor-default select-none relative py-2 pl-8 pr-4`}
-                            >
-                              <span
+                      <Listbox.Options
+                        static
+                        className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                      >
+                        {sidoName.map((sido) => (
+                          <Listbox.Option key={sido} value={sido}>
+                            {({ selected, active }) => (
+                              <div
                                 className={`${
-                                  selected ? 'font-semibold' : 'font-normal'
-                                } block truncate`}
+                                  active ? 'text-white bg-blue-600' : 'text-gray-900'
+                                } cursor-default select-none relative py-2 pl-8 pr-4`}
                               >
-                                {item.stationName}
-                              </span>
-                              {selected && (
                                 <span
                                   className={`${
-                                    active ? 'text-white' : 'text-blue-600'
-                                  } absolute inset-y-0 left-0 flex items-center pl-1.5`}
+                                    selected ? 'font-semibold' : 'font-normal'
+                                  } block truncate`}
                                 >
-                                  <svg
-                                    className="h-5 w-5"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                  >
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                      clipRule="evenodd"
-                                    />
-                                  </svg>
+                                  {sido}
                                 </span>
+                                {selected && (
+                                  <span
+                                    className={`${
+                                      active ? 'text-white' : 'text-blue-600'
+                                    } absolute inset-y-0 left-0 flex items-center pl-1.5`}
+                                  >
+                                    <svg
+                                      className="h-5 w-5"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      viewBox="0 0 20 20"
+                                      fill="currentColor"
+                                    >
+                                      <path
+                                        fillRule="evenodd"
+                                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                        clipRule="evenodd"
+                                      />
+                                    </svg>
+                                  </span>
+                                )}
+                              </div>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </>
+              )}
+            </Listbox>
+          </div>
+
+          {currentLocation.pathname !== '/total' && (
+            <div className="w-full max-w-xs mx-auto">
+              <Listbox
+                as="div"
+                className="space-y-1"
+                value={selectedGugun}
+                onChange={gugunSelectHandler}
+              >
+                {({ open }) => (
+                  <>
+                    <Listbox.Label className="block text-sm leading-5 font-bold text-gray-700">
+                      구군을 선택하세요!
+                    </Listbox.Label>
+                    <div className="relative">
+                      <span className="inline-block w-full rounded-md shadow-sm">
+                        <Listbox.Button className="text-black cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                          <span className="block truncate">
+                            {selectedGugun ? selectedGugun : sidoDustContents[0].stationName}
+                          </span>
+                          <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <svg
+                              className="h-5 w-5 text-gray-400"
+                              viewBox="0 0 20 20"
+                              fill="none"
+                              stroke="currentColor"
+                            >
+                              <path
+                                d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </svg>
+                          </span>
+                        </Listbox.Button>
+                      </span>
+
+                      <Transition
+                        show={open}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        className="absolute mt-1 w-full rounded-md bg-white shadow-lg"
+                      >
+                        <Listbox.Options
+                          static
+                          className="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                        >
+                          {sidoDustContents?.map((item: any, index: number) => (
+                            <Listbox.Option key={index} value={item.stationName}>
+                              {({ selected, active }) => (
+                                <div
+                                  className={`${
+                                    active ? 'text-white bg-blue-600' : 'text-gray-900'
+                                  } cursor-default select-none relative py-2 pl-8 pr-4`}
+                                >
+                                  <span
+                                    className={`${
+                                      selected ? 'font-semibold' : 'font-normal'
+                                    } block truncate`}
+                                  >
+                                    {item.stationName}
+                                  </span>
+                                  {selected && (
+                                    <span
+                                      className={`${
+                                        active ? 'text-white' : 'text-blue-600'
+                                      } absolute inset-y-0 left-0 flex items-center pl-1.5`}
+                                    >
+                                      <svg
+                                        className="h-5 w-5"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                          clipRule="evenodd"
+                                        />
+                                      </svg>
+                                    </span>
+                                  )}
+                                </div>
                               )}
-                            </div>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Transition>
-                </div>
-              </>
-            )}
-          </Listbox>
-        </div>
+                            </Listbox.Option>
+                          ))}
+                        </Listbox.Options>
+                      </Transition>
+                    </div>
+                  </>
+                )}
+              </Listbox>
+            </div>
+          )}
+        </>
       )}
     </div>
   )
